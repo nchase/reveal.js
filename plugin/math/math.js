dde26e12e46a21138249ea83bdd10aa7dfeeb4d3
Yves Delley <hack@delley.net>
2014-09-14 17:36:22 +0200

    give full access to MathJax options, also changed default scale slightly

--- plugin/math/math.js
+++ plugin/math/math.js
@@ -11,12 +11,28 @@ var RevealMath = window.RevealMath || (function(){
 	options.config = options.config || 'TeX-AMS_HTML-full';
 
 	loadScript( options.mathjax + '?config=' + options.config, function() {
-
-		MathJax.Hub.Config({
+        var MJaxConfig = {
 			messageStyle: 'none',
 			tex2jax: { inlineMath: [['$','$'],['\\(','\\)']] },
-			skipStartupTypeset: true
-		});
+			skipStartupTypeset: true,
+            'HTML-CSS': { scale: 88 }
+		};
+        
+        for(var key in options) {
+            if(MJaxConfig[key] !== undefined && options[key] instanceof Object){
+                console.log('merging nested options for ',key,MJaxConfig[key],options[key]);
+                for(var skey in options[key]){
+                    MJaxConfig[key][skey] = options[key][skey];
+                }
+            } else {
+                MJaxConfig[key] = options[key];
+            }
+        }
+            
+        delete MJaxConfig.mathjax;
+        delete MJaxConfig.config;
+        
+		MathJax.Hub.Config(MJaxConfig);
 
 		// Typeset followed by an immediate reveal.js layout since
 		// the typesetting process could affect slide height
